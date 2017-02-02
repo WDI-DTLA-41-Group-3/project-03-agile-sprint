d1abfe931f99ab53fc972ab8c24bef19d2276463
Tim Clark <oeomusic@gmail.com>
2017-02-02 12:02:49 -0800

    seeds user model

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,39 @@
 require('./config')
 // Require models
+const User = require('../models/user')
+
+// User
+// .remove({})
+// .then( () => {
+//   return User.create([
+//     {
+//       Id: 'crimclark',
+//       avatar: 'http://placekitten.com/g/200/300',
+//       blogs: [{content: 'This is my blog'}]
+//     }
+//   ]);
+// })
+// .then( () => {
+//   return User.find({})
+// })
+// .then( (users) => {
+//   console.log(`Seeded ${users.length} users`);
+// })
+// .then( () => {
+//   process.exit() // exit the script
+// });
+
+
+
+User.findOne({ Id: 'crimclark' }, (err, user) => {
+  user.blogs.push({
+    content: 'another blog'
+  })
+  console.log(user);
+  user.save();
+})
+
+
+
+
 
