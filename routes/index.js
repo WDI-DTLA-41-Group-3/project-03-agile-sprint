bf27efe08a59be4a247f637e6be43ed809688a8d
Justin Samuels <blizm87@gmail.com>
2017-02-02 11:11:12 -0800

    Testing title template

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Home'})
+  res.render('index', {title: 'Blog Heaven'});
 })
 
 module.exports = router
