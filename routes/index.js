3ec3a90f7ed27122b7b3a4c48069eca0d3a2279e
Justin Samuels <blizm87@gmail.com>
2017-02-02 17:50:21 -0800

    Gained access through Oauth, need to render active page, json being rendered is user information.

--- routes/index.js
+++ routes/index.js
@@ -13,37 +13,55 @@ router.get('/', (req, res, next) => {
 router.get('/login', (req, res, next) =>{
   const client_id = process.env.GITHUB_CLIENT_ID;
   const redirect_url = 'https://github.com/login/oauth/authorize';
-  const scope = 'profile email';
   const state = 'JJAT';
-  const queryParams = `?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}`;
+  const queryParams = `?client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}`;
     res.redirect(redirect_url + queryParams);
 });
 
 router.get('/authorized', (req, res, next) =>{
   const code = req.query.code;
   const client_id = process.env.GITHUB_CLIENT_ID;
-  const client_secret = process.env. GITHUB_CLIENT_SECRET;
+  const client_secret = process.env.GITHUB_CLIENT_SECRET;
   const state = req.query.state;
 
-  const options = {
-    method: 'POST',
-    url: 'https://github.com/login/oauth/access_token',
+  const json = {
     code: code,
     client_id: client_id,
     client_secret: client_secret,
     redirect_uri: redirect_uri,
     state: state
   }
+
+  const options = {
+    method: 'POST',
+    url: 'https://github.com/login/oauth/access_token',
+    headers: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Awesome-Octocat-App'
+    },
+    json: json
+  }
   request(options, (err, response, body) =>{
+
     if(response.statusCode === 200){
       req.session.access_token = body.access_token;
-      res.redirect('/active');
-    } // else {
-    //   console.log('I am the error: ' + err);
+      const options = {
+        method: 'GET',
+        url: `https://api.github.com/user?access_token=${body.access_token}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'Awesome-Octocat-App'
+        }
+      }
+      request(options, (err, response, body) => {
+        res.json(body);
+      });
+      // res.redirect('/active');
+    }// } // else {
+    // //   console.log('I am the error: ' + err);
     //   res.json(body);
     // }
   });
-
 });
 
 router.get('/active', (req, res, next) => {
